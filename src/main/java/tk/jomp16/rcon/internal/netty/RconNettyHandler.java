5296f529626fc7b92d160c0683bd0eb28b144479
José Olívio M. Pedrosa <joseoliviopedrosa@gmail.com>
2015-08-17 15:44:31 -0300

    Rename project and add GPLv3 copyright.

--- src/main/java/tk/jomp16/rcon/internal/netty/RconNettyHandler.java
+++ src/main/java/tk/jomp16/rcon/internal/netty/RconNettyHandler.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 jomp16
+ *
+ * This file is part of Source RCON - Java.
+ *
+ * Source RCON - Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Source RCON - Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Source RCON - Java. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package tk.jomp16.rcon.internal.netty;
 
 import io.netty.channel.ChannelHandlerContext;
